4fe979eae2647f17ae4386d59cf521ec00536749
mahdieh <mahdiefarahani00@gmail.com>
2025-12-04 16:43:24 +0330

    v4

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-
 import "./i18n";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import Login from "./pages/Login.tsx";
 import Loginp from "./pages/Loginp.tsx";
+import Password from "./pages/Password.tsx";
 
 const router = createBrowserRouter([
   {
@@ -17,6 +17,11 @@ const router = createBrowserRouter([
     path: "/Loginp",
     element: <Loginp />,
   },
+  {
+    path: "/Password",
+    element: <Password />,
+  },
+
 ]);
 
 createRoot(document.getElementById("root")!).render(
